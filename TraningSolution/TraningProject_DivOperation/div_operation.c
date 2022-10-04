917e75acfbfa91b6a5e85c1c44b7356d72f4859f
Dinulka <loonmoon12@gmail.com>
2022-10-05 01:37:28 +0300

    исправлен баг с - 0

--- TraningSolution/TraningProject_DivOperation/div_operation.c
+++ TraningSolution/TraningProject_DivOperation/div_operation.c
@@ -3,13 +3,18 @@ int main() {
 	int a, b;
 	float div;
 	scanf_s("%d %d", &a, &b);
-	if (b == 0) {
-		printf("Ошибка");
-	}
-	else {
-		div = a / b;
-		printf("%f", div);
-	}
+    if (b == 0) {
+        printf("На 0 делить нельзя\n");
+    }
+    else {
+        if (a == 0) {
+            printf("Ответ 0");
+        }
+        else {
+            div = a / b;
+            printf("Ответ - %f", div);
+        }
+    }
 	system("pause");
 	return 0;
 }
